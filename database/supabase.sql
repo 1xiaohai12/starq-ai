a51b3d87f5648a7d8cc09a9149985e4c99898a97
1xiaohai12 <550544942@qq.com>
2026-09-25 15:11:08 +0800

    Delete database directory

--- database/supabase.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE orders(
-id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-order_no varchar(50),
-email varchar(255),
-product varchar(50),
-amount integer,
-status varchar(30),
-created_at timestamp DEFAULT now()
-);
